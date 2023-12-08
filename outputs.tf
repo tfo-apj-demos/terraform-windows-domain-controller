5f4e749fc368f1a564a4782444bb9bd36155c8ea
Aaron Evans <aarone@outlook.com.au>
2023-12-08 22:09:19 +1100

    tf fmt

--- outputs.tf
+++ outputs.tf
@@ -25,5 +25,5 @@ output "guest_id" {
 
 output "nsxt_allocated_ip_address" {
   description = "The IP address from the NSX IP Address Pool"
-  value = nsxt_policy_ip_address_allocation.this.allocation_ip
+  value       = nsxt_policy_ip_address_allocation.this.allocation_ip
 }
