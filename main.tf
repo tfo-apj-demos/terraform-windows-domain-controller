e41d4f1fd759b9b57432db116f0d32b94b8da2bd
Aaron Evans <aarone@outlook.com.au>
2024-01-02 11:06:53 +1100

    Changed to PMR

--- main.tf
+++ main.tf
@@ -16,10 +16,10 @@ resource "nsxt_policy_ip_address_allocation" "this" {
 }
 
 module "vm" {
-  source = "github.com/tfo-apj-demos/terraform-vsphere-virtual-machine"
+  source  = "app.terraform.io/tfo-apj-demos/virtual-machine/vsphere"
+  version = "1.3.6"
 
   template          = data.hcp_packer_image.base-windows-2022.cloud_image_id
-
   datacenter        = var.site
   cluster           = var.environment
   resource_pool     = var.tier
